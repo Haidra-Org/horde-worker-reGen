9b765163b3b2fabf391452bf6b329aaa70d631bc
tazlin <tazlin.on.github@gmail.com>
2023-10-04 16:54:25 -0400

    fix: incorrect condition that snuck in for unload vram logic

--- horde_worker_regen/process_management/process_manager.py
+++ horde_worker_regen/process_management/process_manager.py
@@ -957,9 +957,6 @@ class HordeWorkerProcessManager:
                 if process_info.is_process_busy():
                     continue
 
-                if process_info.last_process_state == process_info.last_process_state:
-                    continue
-
                 if process_info.loaded_horde_model_name is None:
                     continue
 

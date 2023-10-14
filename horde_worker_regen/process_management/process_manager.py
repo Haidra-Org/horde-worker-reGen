b49e5bceceb2c588ec2275424cb363d84e6cc9c0
db0 <mail@dbzer0.com>
2023-10-14 11:35:50 +0200

    fix: avoid flooding output on paused pops

--- horde_worker_regen/process_management/process_manager.py
+++ horde_worker_regen/process_management/process_manager.py
@@ -1582,8 +1582,10 @@ class HordeWorkerProcessManager:
         if self._consecutive_failed_jobs >= 3:
             logger.error(
                 "Too many consecutive failed jobs, pausing job pops. "
-                "Please look into what happened and let the devs know.",
+                "Please look into what happened and let the devs know. ",
+                "Waiting 300 seconds...",
             )
+            time.sleep(300)
             return
 
         if len(self.job_deque) >= self.bridge_data.queue_size + 1:  # FIXME?

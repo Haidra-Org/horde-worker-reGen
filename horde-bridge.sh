8f242649cec04a976424dffb1c6581caa1aaac78
db0 <mail@dbzer0.com>
2024-01-06 16:36:50 +0100

    feat: Use libjemalloc if it exists

--- horde-bridge.sh
+++ horde-bridge.sh
@@ -8,5 +8,9 @@ CONDA_ENV_PATH="$SCRIPT_DIR/conda/envs/linux/lib"
 # Add the Conda environment to LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$CONDA_ENV_PATH:$LD_LIBRARY_PATH"
 
+if [ -f "/usr/lib/x86_64-linux-gnu/libjemalloc.so.2" ]; then
+    export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libjemalloc.so.2"
+fi
+
 ./runtime.sh python -s download_models.py
 ./runtime.sh python -s run_worker.py $*

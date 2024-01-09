bed17b7a3f4e71e29df443891426904461b68526
tazlin <tazlin.on.github@gmail.com>
2024-01-09 11:41:56 -0500

    chore: broadcast that this is from the kaggle branch

--- horde_worker_regen/process_management/process_manager.py
+++ horde_worker_regen/process_management/process_manager.py
@@ -1739,7 +1739,7 @@ class HordeWorkerProcessManager:
             job_pop_request = ImageGenerateJobPopRequest(
                 apikey=self.bridge_data.api_key,
                 name=self.bridge_data.dreamer_worker_name,
-                bridge_agent="AI Horde Worker reGen:2:https://github.com/Haidra-Org/horde-worker-reGen/#20231009_3",
+                bridge_agent="AI Horde Worker reGen:2:https://github.com/Haidra-Org/horde-worker-reGen/#20231009_3-kaggle",
                 bridge_version=2,
                 models=models_to_pop,
                 nsfw=self.bridge_data.nsfw,

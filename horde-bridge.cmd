6953805bb0bea22aaa3c41e6236331c830bdc652
db0 <mail@dbzer0.com>
2024-01-27 18:16:36 +0100

    fix: removed abort on windows for now

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,9 +1,9 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
-if ERRORLEVEL 1 GOTO ABORT
+@REM if ERRORLEVEL 1 GOTO ABORT
 echo "Model Download OK. Starting worker..."
 call runtime python -s run_worker.py %*
 
-:ABORT
-echo "download_models.py exited with error code. Aborting"
+@REM :ABORT
+@REM echo "download_models.py exited with error code. Aborting"

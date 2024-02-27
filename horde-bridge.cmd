81fb00609e4e7864236bee18092322acf31724e3
tazlin <tazlin.on.github@gmail.com>
2024-02-27 16:22:47 -0500

    fix: side-step deactivate issues in detecting errorlevel

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,12 +1,16 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
-@REM if ERRORLEVEL 1 GOTO ABORT
+if %ERRORLEVEL% NEQ 0 GOTO ABORT
 echo "Model Download OK. Starting worker..."
 call runtime python -s run_worker.py %*
 
-@REM :ABORT
-@REM echo "download_models.py exited with error code. Aborting"
+GOTO END
 
-# Press any key to continue . . .
+:ABORT
+echo "download_models.py exited with error code. Aborting"
+
+:END
+call micromamba deactivate >nul
+call deactivate >nul
 pause
--- preload-models.cmd
+++ preload-models.cmd
@@ -1,4 +1,6 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
+call micromamba deactivate >nul
+call deactivate >nul
 pause

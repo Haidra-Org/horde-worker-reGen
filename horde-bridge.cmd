930180a607dd0c74b8d14f819a2f83625e14aec8
tazlin <tazlin.on.github@gmail.com>
2024-02-27 16:14:03 -0500

    fix: revert errorlevel checking

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,14 +1,12 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
-if %ERRORLEVEL% == 1 GOTO ABORT
+@REM if ERRORLEVEL 1 GOTO ABORT
 echo "Model Download OK. Starting worker..."
 call runtime python -s run_worker.py %*
 
-GOTO END
+@REM :ABORT
+@REM echo "download_models.py exited with error code. Aborting"
 
-:ABORT
-echo "download_models.py exited with error code. Aborting"
-
-:END
+# Press any key to continue . . .
 pause

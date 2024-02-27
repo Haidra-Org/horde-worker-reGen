bbfa482c4bc7482246bd7badb024c7361f051635
tazlin <tazlin.on.github@gmail.com>
2024-02-27 15:50:34 -0500

    fix: better download/validation logic

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,12 +1,14 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
-@REM if ERRORLEVEL 1 GOTO ABORT
+if ERRORLEVEL 1 GOTO ABORT
 echo "Model Download OK. Starting worker..."
 call runtime python -s run_worker.py %*
 
-@REM :ABORT
-@REM echo "download_models.py exited with error code. Aborting"
+GOTO END
 
-# Press any key to continue . . .
+:ABORT
+echo "download_models.py exited with error code. Aborting"
+
+:END
 pause

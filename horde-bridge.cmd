9efcc0e079125fabdb16e8d63e5eebfae713b1b4
tazlin <tazlin.on.github@gmail.com>
2024-02-27 16:10:19 -0500

    fix: different errorlevel checking

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 call runtime python -s download_models.py
-if ERRORLEVEL 1 GOTO ABORT
+if %ERRORLEVEL% == 1 GOTO ABORT
 echo "Model Download OK. Starting worker..."
 call runtime python -s run_worker.py %*
 

52ee7fdd18a96acd46b11f446c10a857361d21d0
tazlin <tazlin.on.github@gmail.com>
2024-03-01 12:10:29 -0500

    fix: try force highvram mode

--- horde_worker_regen/process_management/worker_entry_points.py
+++ horde_worker_regen/process_management/worker_entry_points.py
@@ -51,7 +51,12 @@ def start_inference_process(
                     process_id=process_id,
                     logging_verbosity=0,
                     force_normal_vram_mode=not high_memory_mode,
-                    extra_comfyui_args=["--disable-smart-memory"],
+                    extra_comfyui_args=["--disable-smart-memory"]
+                    if not high_memory_mode
+                    else [
+                        "--disable-smart-memory",
+                        "--highvram",
+                    ],
                 )
         except Exception as e:
             logger.critical(f"Failed to initialise hordelib: {type(e).__name__} {e}")

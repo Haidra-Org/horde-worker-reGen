5cb6b352e070bc6f378fb5ff80e570af64849a83
tazlin <tazlin.on.github@gmail.com>
2024-03-03 11:03:00 -0500

    fix: update horde dependency patches on worker start

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """Configures pytest and creates fixtures."""
 # import hordelib
+from pathlib import Path
+
 import pytest
 from loguru import logger
 
@@ -8,3 +10,28 @@ from loguru import logger
 def init_hordelib() -> None:
     # hordelib.initialise()
     logger.warning("hordelib.initialise() not called")
+
+
+PRECOMMIT_FILE_PATH = Path(__file__).parent.parent / ".pre-commit-config.yaml"
+REQUIREMENTS_FILE_PATH = Path(__file__).parent.parent / "requirements.txt"
+
+TRACKED_DEPENDENCIES = [
+    "horde_sdk",
+    "hordelib",
+    "horde_model_reference",
+    # "horde_safety"
+]
+
+
+@pytest.fixture(scope="session")
+def horde_dependency_versions() -> list[tuple[str, str]]:
+    with open(REQUIREMENTS_FILE_PATH) as f:
+        requirements = f.readlines()
+
+    dependencies = []
+    for req in requirements:
+        for dep in TRACKED_DEPENDENCIES:
+            if req.startswith(dep):
+                dependencies.append((dep, req.split("~=")[1].strip()))
+
+    return dependencies

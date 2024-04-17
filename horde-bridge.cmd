7a799d01d8a9d08a5cebd65e75cfa2f0d3e9b231
mrschmiklz <33757454+mrschmiklz@users.noreply.github.com>
2024-04-17 09:38:49 -0600

    Update horde-bridge.cmd

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,6 +1,20 @@
 @echo off
 cd /d %~dp0
 
+:: Check if AI_HORDE_URL is set and if it is not, set it
+if "%AI_HORDE_URL%"=="" (
+    echo Setting AI_HORDE_URL environment variable
+    set AI_HORDE_URL=https://api.aipowergrid.io/api/
+) else (
+    echo AI_HORDE_URL is already set to %AI_HORDE_URL%
+)
+
+:: Ask for GPU selection
+echo Select which GPU to use by index (e.g., 0, 1, 2...):
+set /p GPU_INDEX="Enter GPU index: "
+set CUDA_VISIBLE_DEVICES=%GPU_INDEX%
+echo Using GPU %CUDA_VISIBLE_DEVICES%
+
 : This first call to runtime activates the environment for the rest of the script
 call runtime python -s -m pip -V
 
@@ -9,6 +23,11 @@ if %ERRORLEVEL% NEQ 0 (
     echo "Please run update-runtime.cmd."
     GOTO END
 )
+
+:: Change constants in path_consts.py
+echo Modifying path_consts.py to update repository details...
+call python update_path_consts.py
+
 
 call python -s -m pip check
 if %ERRORLEVEL% NEQ 0 (

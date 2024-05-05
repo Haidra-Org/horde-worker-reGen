82f31d49a3a578b99919a71489d04ecace487ac9
tazlin <tazlin.on.github@gmail.com>
2024-05-05 11:46:50 -0400

    chore: `hordelib` name change to `horde-engine`

--- horde_worker_regen/__init__.py
+++ horde_worker_regen/__init__.py
@@ -9,3 +9,21 @@ from pathlib import Path  # noqa: E402
 ASSETS_FOLDER_PATH = Path(__file__).parent / "assets"
 
 __version__ = "5.0.2"
+
+
+import pkg_resources  # noqa: E402
+
+
+def check_hordelib_not_installed() -> None:
+    """Check that hordelib is not installed."""
+    try:
+        pkg_resources.get_distribution("hordelib")
+        raise RuntimeError(
+            "hordelib is installed. Please uninstall it before running this package. "
+            "`hordelib` has been renamed to `horde-engine`.",
+        )
+    except pkg_resources.DistributionNotFound:
+        pass
+
+
+check_hordelib_not_installed()

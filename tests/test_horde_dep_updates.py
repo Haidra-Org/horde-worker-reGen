4dee6d01cf8d93fc9ba2a1858978e7646cce0bc1
tazlin <tazlin.on.github@gmail.com>
2024-05-05 12:57:27 -0400

    tests/fix: constrain update checks to `horde_*` deps

--- tests/test_horde_dep_updates.py
+++ tests/test_horde_dep_updates.py
@@ -5,6 +5,9 @@ HORDE_BRIDGE_SCRIPT = Path(__file__).parent.parent / "horde-bridge.cmd"
 
 def test_horde_bridge_updating(horde_dependency_versions: list[tuple[str, str]]) -> None:
     """Check that the versions of horde deps. in horde-bridge.cmd match the versions in requirements.txt."""
+    haidra_dep_string = "horde"
+    haidra_deps = [(dep, version) for dep, version in horde_dependency_versions if haidra_dep_string in dep]
+
     script_lines = HORDE_BRIDGE_SCRIPT.read_text().split("\n")
 
     found_line = False
@@ -12,7 +15,7 @@ def test_horde_bridge_updating(horde_dependency_versions: list[tuple[str, str]])
         if "python -s -m pip install" in line:
             found_line = True
             assert "-U" in line, "No -U flag found in pip install command"
-            for dep, version in horde_dependency_versions:
+            for dep, version in haidra_deps:
                 assert f"{dep}~={version}" in line, f"Dependency {dep} not found in pip install command"
 
     assert found_line

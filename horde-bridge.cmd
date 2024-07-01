d772a2bc81f695fdb9ce1673a2f66500d7277a4c
tazlin <tazlin.on.github@gmail.com>
2024-07-01 11:53:20 -0400

    chore: match pinned horde deps across all files

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -5,7 +5,7 @@ cd /d %~dp0
 call runtime python -s -m pip -V
 
 call python -s -m pip uninstall hordelib
-call python -s -m pip install horde_sdk~=0.10.0 horde_model_reference~=0.6.3 horde_engine~=2.11.1 horde_safety~=0.2.3 -U
+call python -s -m pip install horde_sdk~=0.13.0 horde_model_reference~=0.7.0 horde_engine~=2.12.0 horde_safety~=0.2.3 -U
 
 if %ERRORLEVEL% NEQ 0 (
     echo "Please run update-runtime.cmd."

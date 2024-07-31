be3bb3f3db2fca46c6d5808f86f66c5b15af9cd5
tazlin <tazlin.on.github@gmail.com>
2024-07-31 11:28:44 -0400

    tests: fix tuple/dict mix up in horde dep test

--- tests/test_horde_dep_updates.py
+++ tests/test_horde_dep_updates.py
@@ -3,10 +3,10 @@ from pathlib import Path
 HORDE_BRIDGE_SCRIPT = Path(__file__).parent.parent / "horde-bridge.cmd"
 
 
-def test_horde_bridge_updating(horde_dependency_versions: list[tuple[str, str]]) -> None:
+def test_horde_bridge_updating(horde_dependency_versions: dict[str, str]) -> None:
     """Check that the versions of horde deps. in horde-bridge.cmd match the versions in requirements.txt."""
     haidra_dep_string = "horde"
-    haidra_deps = [(dep, version) for dep, version in horde_dependency_versions if haidra_dep_string in dep]
+    haidra_deps = [(dep, version) for dep, version in horde_dependency_versions.items() if haidra_dep_string in dep]
 
     script_lines = HORDE_BRIDGE_SCRIPT.read_text().split("\n")
 

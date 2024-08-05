3af4fe30c72130b9890a2d8a9ce802d543a925d6
tazlin <tazlin.on.github@gmail.com>
2024-08-05 19:44:37 -0400

    fix: run conda commands relative to linux script
    This allows your PWD do be used to isolate multiple configs and sets of logs when running more than one worker instance on a single machine

--- update-runtime.sh
+++ update-runtime.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ignore_hordelib=false
 
@@ -26,15 +27,15 @@ done
 
 CONDA_ENVIRONMENT_FILE=environment.yaml
 
-wget -qO- https://github.com/mamba-org/micromamba-releases/releases/latest/download/micromamba-linux-64.tar.bz2 | tar -xvj bin/micromamba
-if [ ! -f "conda/envs/linux/bin/python" ]; then
-    bin/micromamba create --no-shortcuts -r conda -n linux -f ${CONDA_ENVIRONMENT_FILE} -y
+wget -qO- https://github.com/mamba-org/micromamba-releases/releases/latest/download/micromamba-linux-64.tar.bz2 | tar -xvj "$SCRIPT_DIR/bin/micromamba"
+if [ ! -f "$SCRIPT_DIR/conda/envs/linux/bin/python" ]; then
+    bin/micromamba create --no-shortcuts -r "$SCRIPT_DIR/conda" -n linux -f ${CONDA_ENVIRONMENT_FILE} -y
 fi
-bin/micromamba create --no-shortcuts -r conda -n linux -f ${CONDA_ENVIRONMENT_FILE} -y
+bin/micromamba create --no-shortcuts -r "$SCRIPT_DIR/conda" -n linux -f ${CONDA_ENVIRONMENT_FILE} -y
 
 if [ "$hordelib" = true ]; then
- bin/micromamba run -r conda -n linux python -s -m pip uninstall -y hordelib horde_engine horde_sdk horde_model_reference
- bin/micromamba run -r conda -n linux python -s -m pip install horde_engine horde_model_reference --extra-index-url https://download.pytorch.org/whl/cu121
+ bin/micromamba run -r "$SCRIPT_DIR/conda" -n linux python -s -m pip uninstall -y hordelib horde_engine horde_sdk horde_model_reference
+ bin/micromamba run -r "$SCRIPT_DIR/conda" -n linux python -s -m pip install horde_engine horde_model_reference --extra-index-url https://download.pytorch.org/whl/cu121
 else
- bin/micromamba run -r conda -n linux python -s -m pip install -r requirements.txt -U --extra-index-url https://download.pytorch.org/whl/cu121
+ bin/micromamba run -r "$SCRIPT_DIR/conda" -n linux python -s -m pip install -r "$SCRIPT_DIR/requirements.txt" -U --extra-index-url https://download.pytorch.org/whl/cu121
 fi

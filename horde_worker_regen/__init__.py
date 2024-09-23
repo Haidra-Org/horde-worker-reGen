1c8f0eead0796b3460d599d643e9684818ab7a47
tazlin <tazlin.on.github@gmail.com>
2024-09-23 10:40:15 -0400

    chore: version bump

--- horde_worker_regen/__init__.py
+++ horde_worker_regen/__init__.py
@@ -8,7 +8,7 @@ from pathlib import Path  # noqa: E402
 
 ASSETS_FOLDER_PATH = Path(__file__).parent / "assets"
 
-__version__ = "9.0.1"
+__version__ = "9.0.2"
 
 
 import pkg_resources  # noqa: E402

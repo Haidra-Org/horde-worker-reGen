a20a90e9629df34aca7f9783a89a12af9118dab4
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-10-18 00:42:35 +0200

    fix: rocm version via index url (#313)
    * rocm version via index url
    
    --extra-index-url https://download.pytorch.org/whl/rocmX.Y is sufficient to install the right package.
    This also makes A/B version testing easier.
    
    * build/fix: bring rocm reqs.txt in line with main reqs.txt
    
    * include wheel
    
    If this is not included, the flash_attn build fails on ROCm cards
    
    * Turn requirements.rocm.txt into a symlink
    
    This is a temporary measure for backwards compatibility. Once all scripts and Dockerfiles are updated it can be removed.
    
    * remove requirements.rocm.txt from scripts
    
    ---------
    
    Co-authored-by: tazlin <tazlin.on.github@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,6 @@ def init_hordelib() -> None:
 
 PRECOMMIT_FILE_PATH = Path(__file__).parent.parent / ".pre-commit-config.yaml"
 REQUIREMENTS_FILE_PATH = Path(__file__).parent.parent / "requirements.txt"
-ROCM_REQUIREMENTS_FILE_PATH = Path(__file__).parent.parent / "requirements.rocm.txt"
 
 TRACKED_DEPENDENCIES = [
     "horde_sdk",
@@ -38,32 +37,6 @@ def tracked_dependencies() -> list[str]:
 def horde_dependency_versions() -> dict[str, str]:
     """Get the versions of horde dependencies from the requirements file."""
     with open(REQUIREMENTS_FILE_PATH) as f:
-        requirements = f.readlines()
-
-    dependencies = {}
-    for req in requirements:
-        for dep in TRACKED_DEPENDENCIES:
-            if req.startswith(dep):
-                if "==" in req:
-                    version = req.split("==")[1].strip()
-                elif "~=" in req:
-                    version = req.split("~=")[1].strip()
-                elif ">=" in req:
-                    version = req.split(">=")[1].strip()
-                else:
-                    raise ValueError(f"Unsupported version pin: {req}")
-
-                # Strip any info starting from the `+` character
-                version = version.split("+")[0]
-                dependencies[dep] = version
-
-    return dependencies
-
-
-@pytest.fixture(scope="session")
-def rocm_horde_dependency_versions() -> dict[str, str]:
-    """Get the versions of horde dependencies from the ROCm requirements file."""
-    with open(ROCM_REQUIREMENTS_FILE_PATH) as f:
         requirements = f.readlines()
 
     dependencies = {}

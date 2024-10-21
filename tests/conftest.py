3f3e7fad7e4c86763319f016c8544df18526c263
tazlin <tazlin.on.github@gmail.com>
2024-10-21 11:06:30 -0400

    tests: readd rocm file check

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,7 @@ def init_hordelib() -> None:
 
 PRECOMMIT_FILE_PATH = Path(__file__).parent.parent / ".pre-commit-config.yaml"
 REQUIREMENTS_FILE_PATH = Path(__file__).parent.parent / "requirements.txt"
+ROCM_REQUIREMENTS_FILE_PATH = Path(__file__).parent.parent / "requirements.rocm.txt"
 
 TRACKED_DEPENDENCIES = [
     "horde_sdk",
@@ -37,6 +38,32 @@ def tracked_dependencies() -> list[str]:
 def horde_dependency_versions() -> dict[str, str]:
     """Get the versions of horde dependencies from the requirements file."""
     with open(REQUIREMENTS_FILE_PATH) as f:
+        requirements = f.readlines()
+
+    dependencies = {}
+    for req in requirements:
+        for dep in TRACKED_DEPENDENCIES:
+            if req.startswith(dep):
+                if "==" in req:
+                    version = req.split("==")[1].strip()
+                elif "~=" in req:
+                    version = req.split("~=")[1].strip()
+                elif ">=" in req:
+                    version = req.split(">=")[1].strip()
+                else:
+                    raise ValueError(f"Unsupported version pin: {req}")
+
+                # Strip any info starting from the `+` character
+                version = version.split("+")[0]
+                dependencies[dep] = version
+
+    return dependencies
+
+
+@pytest.fixture(scope="session")
+def rocm_horde_dependency_versions() -> dict[str, str]:
+    """Get the versions of horde dependencies from the ROCm requirements file."""
+    with open(ROCM_REQUIREMENTS_FILE_PATH) as f:
         requirements = f.readlines()
 
     dependencies = {}

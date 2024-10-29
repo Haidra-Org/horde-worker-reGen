ee7de48f64e413f5a52ea6ef418282bd76c85090
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-10-29 15:28:20 +0100

    check env variable

--- Dockerfiles/setup_rocm.sh
+++ Dockerfiles/setup_rocm.sh
@@ -2,4 +2,6 @@
 . venv/bin/activate
 python -m pip uninstall -y pynvml nvidia-ml-py
 
-./horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
+if [[ "${FLASH_ATTENTION_USE_TRITON_ROCM^^}" == "TRUE" ]]; then #this requires bash, not sh to work
+  ./horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
+fi

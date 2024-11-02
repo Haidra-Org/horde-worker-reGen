1d7c5015cf3168fac456ab4c5c625755f804272b
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-11-02 15:46:19 +0100

    check is in install_amd_go_fast.sh

--- Dockerfiles/setup_rocm.sh
+++ Dockerfiles/setup_rocm.sh
@@ -2,6 +2,4 @@
 . venv/bin/activate
 python -m pip uninstall -y pynvml nvidia-ml-py
 
-if [[ "${FLASH_ATTENTION_USE_TRITON_ROCM^^}" == "TRUE" ]]; then #this requires bash, not sh to work
-  ./horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
-fi
+./horde_worker_regen/amd_go_fast/install_amd_go_fast.sh

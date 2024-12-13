30d94659ca5e4a14a772c6b42d7b1e4bada3838e
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-12-13 08:39:55 +0100

    move PY_SITE_DIR

--- horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
+++ horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+PY_SITE_DIR=$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")
 
 if [ "${FLASH_ATTENTION_TRITON_AMD_ENABLE^^}" == "TRUE" ]; then
 	if ! pip install -U pytest git+https://github.com/Dao-AILab/flash-attention; then
 		echo "Tried to install flash attention and failed!"
 	else
 		echo "Installed flash attn."
-		PY_SITE_DIR=$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")
 		if ! cp horde_worker_regen/amd_go_fast/amd_go_fast.py "${PY_SITE_DIR}"/hordelib/nodes/; then
 			echo "Failed to install AMD GO FAST."
 		else

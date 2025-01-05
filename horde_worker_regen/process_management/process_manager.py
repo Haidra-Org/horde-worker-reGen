3d24a9db8e528f5d731f247bf6af90d5896e5430
tazlin <tazlin.on.github@gmail.com>
2025-01-04 22:00:03 -0500

    fix: don't block extra thread unless batch is actually running

--- horde_worker_regen/process_management/process_manager.py
+++ horde_worker_regen/process_management/process_manager.py
@@ -586,7 +586,7 @@ class ProcessMap(dict[int, HordeProcessInfo]):
         """
         for p in self.values():
             # We only parallelizing if we have a currently running inference with n_iter > 1
-            if p.batch_amount > 1:
+            if p.batch_amount > 1 and p.last_process_state == HordeProcessState.INFERENCE_STARTING:
                 return True, "Batched job"
 
             if (

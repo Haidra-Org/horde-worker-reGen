e370511842e817305edc201549dce05a7e65b83d
tazlin <tazlin.on.github@gmail.com>
2025-01-07 12:07:44 -0500

    fix: matched horde_sdk dep pins

--- horde-bridge-directml.cmd
+++ horde-bridge-directml.cmd
@@ -5,7 +5,7 @@ cd /d %~dp0
 call runtime python -s -m pip -V
 
 call python -s -m pip uninstall hordelib
-call python -s -m pip install horde_sdk~=0.16.4 horde_model_reference~=0.9.1 horde_engine~=2.18.1 horde_safety~=0.2.3 -U
+call python -s -m pip install horde_sdk~=0.17.0 horde_model_reference~=0.9.1 horde_engine~=2.18.1 horde_safety~=0.2.3 -U
 
 if %ERRORLEVEL% NEQ 0 (
     echo "Please run update-runtime.cmd."

d70999e4bda1bce5b4b56a0ab1cf0732aa474abd
tazlin <tazlin.on.github@gmail.com>
2025-01-07 09:19:06 -0500

    fix: punt on more lora download failures
    See also https://github.com/Haidra-Org/hordelib/pull/394

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -5,7 +5,7 @@ cd /d %~dp0
 call runtime python -s -m pip -V
 
 call python -s -m pip uninstall hordelib
-call python -s -m pip install horde_sdk~=0.16.5 horde_model_reference~=0.9.2 horde_engine~=2.20.11 horde_safety~=0.2.3 -U
+call python -s -m pip install horde_sdk~=0.16.5 horde_model_reference~=0.9.2 horde_engine~=2.20.12 horde_safety~=0.2.3 -U
 
 if %ERRORLEVEL% NEQ 0 (
     echo "Please run update-runtime.cmd."
